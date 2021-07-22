d4c8b65a7f794e41f73b8f9dd136b7f72b88d0ba
KevinoCodes <Kevin.Han@ucc.on.ca>
2021-07-22 15:10:24 -0400

    Created Initial UI

---
+++ Public/scripts/auth.js
@@ -0,0 +1,17 @@
+function registerAccount() {
+    firebase.auth().createUserWithEmailAndPassword($('#email').val(), $('#password').val())
+    .then(async (userCredential) => {
+  
+      await firebase.auth().currentUser.updateProfile({
+        displayName: $('#name').val(),
+      })
+  
+      window.setTimeout(() => {
+        window.location.replace('dashboard.html')
+      }, 700)  })
+    .catch((error) => {
+      alert(error.message)
+    });
+    alert("hello")
+  }
+  
